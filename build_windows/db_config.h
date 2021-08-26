1c04d16d0a5026059c42355c74b676f1194e0952
Kelvin Lee <kiyolee@gmail.com>
2021-08-26 17:57:50 +1000

    VS2010 or later: Have stdint.h

--- build_windows/db_config.h
+++ build_windows/db_config.h
@@ -460,7 +460,11 @@
 #define HAVE_STATISTICS 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
+#if defined(_MSC_VER) && _MSC_VER >= 1600
+#define HAVE_STDINT_H 1
+#else
 /* #undef HAVE_STDINT_H */
+#endif
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
